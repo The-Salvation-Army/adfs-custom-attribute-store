35489d2cc363bf0f28c10645569da8d849645005
tim.siney <tim_siney@salvationarmy.org>
2019-01-08 11:56:24 +0000

    Updated unit test

--- AttributeStoreTest/UnitTest1.cs
+++ AttributeStoreTest/UnitTest1.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CustomLdapAttributeStore;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace AttributeStoreTest
 {
@@ -14,19 +15,20 @@ namespace AttributeStoreTest
         [TestInitialize]
         public void Initialize()
         {
-           
-            this.attr.Initialize(null);
-           
-
+            Dictionary<string, string> config = new Dictionary<string, string>()
+            {
+                {"host", "YOUR HOST" },
+                {"port", "636" },
+                { "user", "YOUR USERNAME"},
+                { "password", "YOUR PASSWORD"}
+            };
+            this.attr.Initialize(config);
         }
 
         [TestMethod]
         public void TestLdapSearch()
         {
             attr.BeginExecuteQuery("ldapGroups", new string[] { "test@test.com" }, null, null);
-                 
-                
-
         }
     }
 }
